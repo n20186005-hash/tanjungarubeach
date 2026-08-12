bdc5dd0cd0cc33df8d00d4ddbc09f58c713fc3be
n20186005-hash <n20186005@gmail.com>
2026-08-12 20:39:02 +0800

    更新

--- src/data/site.ts
+++ src/data/site.ts
@@ -20,17 +20,17 @@ export const SITE = {
   lng: 116.0468751,
   mapsUrl: 'https://maps.app.goo.gl/APJN8X1k5SuooYUa6',
   mapsEmbedSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24492.239983766092!2d116.03582442260846!3d5.944619347012934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x323b68293d0d6b37%3A0x178fc96d3fcff4ac!5e0!3m2!1sms!2smy!4v1786528804732!5m2!1sms!2smy',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24492.239983766092!2d116.03582442260846!3d5.944619347012934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x323b68293d0d6b37%3A0x178fc96d3fcff4ac!2zVGFuanVuZyBBcnUgQmVhY2g!5e1!3m2!1sen!2sus!4v1786528804732!5m2!1sen!2sus',
   hours: 'Buka 24 jam, setiap hari',
   rating: 4.5,
   reviewCount: 47665,
   entranceFee: 'Percuma (tiada tiket masuk)',
   ga4Id: 'G-HXM22WWPKP',
   heroImage: '/gallery/pantai-tanjung-aru-14.jpg',
-  galleryCount: 17,
+  galleryCount: 16,
 } as const;
 
 export const GALLERY_IMAGES = Array.from(
   { length: SITE.galleryCount },
-  (_, i) => `/gallery/pantai-tanjung-aru-${i + 1}.jpg`,
+  (_, i) => `/gallery/pantai-tanjung-aru-${i + 2}.jpg`,
 );
